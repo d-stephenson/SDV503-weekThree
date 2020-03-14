46cc3c19f6a254e36a9fd165cf3dfbea2353515a
Dale Stephenson <dalestephenson@zohomail.com>
2020-03-14 17:13:51 +1300

    Playing with functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function product(num1, num2) {
+    return num1*num2;
+}
+console.log("The Product is: ", product(5, 10));
+
+var product = function(num1, num2) {
+    return num1*num2;
+}
+console.log(product(5, 10))
+
+
