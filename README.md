f5f69c845a081951df3ee11fe0e1b312b037e60d
Dale Stephenson <dalestephenson@zohomail.com>
2020-03-16 13:51:01 +1300

    class work 1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SDV503-weekThree
  
+ Answer:
+ '24' != 24 //False
+ != means not equals but is not strict 
+ so '24' does equal 24 (String is converted to number)
+ so 'does not equal' is false because it does equal 
+ if it was:
+  '24' !== 24 then it would be //True
+  because in strict operator '24' does not equal 24 (String is NOT converted to number)
+
+
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function product(num1, num2) {
+/*function product(num1, num2) {
     return num1*num2;
 }
 console.log("The Product is: ", product(5, 10));
@@ -6,6 +6,102 @@ console.log("The Product is: ", product(5, 10));
 var product = function(num1, num2) {
     return num1*num2;
 }
-console.log(product(5, 10))
+console.log(product(5, 10))*/
+
+/*let red = 0;
+let amber = 1;
+let light = 1;
+
+if (light < red) {
+    console.log("STOP"); 
+} else if (light > amber) {
+    console.log("GET READY");
+} else {
+    console.log("GO");
+}*/
+
+/*const a = { isHavingFun: true }
+const b = { isHavingFun: true } 
+console.log(a === b);
+console.log(a == b);
+
+const a = { isHavingFun: true }
+const b = a
+console.log(a === b);
+
+const hasApples = false
+if (hasApples) {
+    console.log("Eat apples")
+} else {
+    console.log("Buy apples")
+}
+
+let apples = 5;
+
+if (apples > 3) {
+    console.log('You have enough apples');  
+}
+else if (apples <= 3 && apples >= 1) {
+    console.log('Put apples on shopping list');
+}
+else {
+    console.log('Buy apples now');
+    
+}
+
+const person = 'Peter';
+switch (person) {
+    case 'Jane':
+        console.log('Hi Jane');
+        break;
+    case 'John':
+        console.log('Hi John');
+        break;
+    case 'Joe':
+        console.log('Hi Joe');
+        break;
+    case "Mary":
+        console.log('Hi Mary');
+        break;
+    default:
+        console.log('Hi');
+        break;
+    }*/
+
+const foo = -5;
+switch (foo) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        console.log('I like this number');
+        break;
+    case -1:
+    case -2:
+        console.log('I love this number');
+        break;
+    default:
+        console.log('I don\'t like this number.');      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
